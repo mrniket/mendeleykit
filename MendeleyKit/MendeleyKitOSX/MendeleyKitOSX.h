8016cf14b030af387f58d108547594b4cc602b1f
Peter Schmidt <peter.schmidt@mendeley.com>
2014-11-11 16:18:44 +0000

    added a MendeleyKitOSX class

---
+++ MendeleyKit/MendeleyKitOSX/MendeleyKitOSX.h
@@ -0,0 +1,13 @@
+//
+//  MendeleyKitOSX.h
+//  MendeleyKitOSX
+//
+//  Created by Peter Schmidt on 07/11/2014.
+//  Copyright (c) 2014 Mendeley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MendeleyKitOSX : NSObject
+
+@end
---
+++ MendeleyKit/MendeleyKitOSX/MendeleyKitOSX.m
@@ -0,0 +1,13 @@
+//
+//  MendeleyKitOSX.m
+//  MendeleyKitOSX
+//
+//  Created by Peter Schmidt on 07/11/2014.
+//  Copyright (c) 2014 Mendeley. All rights reserved.
+//
+
+#import "MendeleyKitOSX.h"
+
+@implementation MendeleyKitOSX
+
+@end
